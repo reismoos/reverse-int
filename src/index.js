d332626763ef5abfc856569f274cfa77d7286ee8
Dmitry <Reismoos@gmail.om>
2022-08-11 12:47:02 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n<0){
+        n = -n
+      }
+  let result='';
+  for (let i = String(n).length - 1; i >= 0; i--) {
+    result += String(n)[i];
+  }
+  return +result;
 }
